69d7993db44bbcfd92a3215f01d60dce71451490
Brandon Graybeal <bmgrayb@gmail.com>
2015-03-11 21:05:15 -0500

    Getting weather requested and displayed

--- www/js/main.js
+++ www/js/main.js
@@ -1,11 +1,13 @@
-function sayHello(){
+function getWeather(){
 		var city = $('#city').val();
 		//window.location.href = "#aboutScreen";
 		$.ajax({
-		    url:'http://api.openweathermap.org/data/2.5/forecast?q=London',
+		    url:'http://api.openweathermap.org/data/2.5/forecast/daily?q='+city+',us&cnt=5&mode=json',
 		}).done(function(data){
 			var results = parseFeed(data,5);
-			alert(results[0][0]);
+
+      display(results, city);
+
 		});
 }
 
@@ -14,20 +16,31 @@ var parseFeed = function(data, quantity) {
   var items = [];
   for(var i = 0; i < quantity; i++) {
     // Always upper case the description string
-    var title = data.list[i].weather[0].main;
-    title = title.charAt(0).toUpperCase() + title.substring(1);
+    var weather = data.list[i].weather[0].main;
+    weather = weather.charAt(0).toUpperCase() + weather.substring(1);
 
-    // Get date/time substring
-    var time = data.list[i].dt_txt;
-    time = time.substring(time.indexOf('-') + 1, time.indexOf(':') + 3);
+    var temp = data.list[i].temp.day;
+    var fahr = (temp - 273.15)*1.8 + 32;
 
-    // Add to menu items array
     items.push({
-      title:title,
-      subtitle:time
+      weather:weather,
+      temp:Math.round(fahr)
     });
   }
 
   // Finally return whole array
   return items;
 };
+
+function display(results, city){
+$('#loc').replaceWith('<h3 id="loc" name="loc">Forecast for ' + city + '</h3>');
+
+  var display = '<table align="center" id="display" name="display" data-role="table" class="ui-responsive"><tr><th>Forecast</th><th>Temperature</th></tr>';
+  display += '<tr><td>' + results[0].weather + '</td><td>' + results[0].temp + '</td></tr>';
+  display += '<tr><td>' + results[1].weather + '</td><td>' + results[1].temp + '</td></tr>';
+  display += '<tr><td>' + results[2].weather + '</td><td>' + results[2].temp + '</td></tr>';
+  display += '<tr><td>' + results[3].weather + '</td><td>' + results[3].temp + '</td></tr>';
+  display += '<tr><td>' + results[4].weather + '</td><td>' + results[4].temp + '</td></tr>';
+  display += '</table>';
+  $('#display').replaceWith(display);
+}
