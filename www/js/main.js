cd1b5507b987aeccf5e04ffec3122f774ae1f353
Brandon Graybeal <bmgrayb@gmail.com>
2015-03-11 22:13:20 -0500

    Weather working

--- www/js/main.js
+++ www/js/main.js
@@ -18,13 +18,23 @@ var parseFeed = function(data, quantity) {
     // Always upper case the description string
     var weather = data.list[i].weather[0].main;
     weather = weather.charAt(0).toUpperCase() + weather.substring(1);
-
+    var desc = data.list[i].weather[0].description;
+    desc = desc.charAt(0).toUpperCase() + desc.substring(1);
+    var time = data.list[i].dt;
+    var d = new Date(0);
+    d.setUTCSeconds(time);
+    var day = getDayName(d);
     var temp = data.list[i].temp.day;
     var fahr = (temp - 273.15)*1.8 + 32;
+    var icon = data.list[i].weather[0].icon;
+    icon = $.trim(icon);
 
     items.push({
       weather:weather,
-      temp:Math.round(fahr)
+      desc:desc,
+      temp:Math.round(fahr),
+      icon:icon,
+      time:day
     });
   }
 
@@ -32,7 +42,35 @@ var parseFeed = function(data, quantity) {
   return items;
 };
 
-function display(results, city){
+function getDayName(d){
+  var weekday = new Array(7);
+  weekday[0]=  "Sun";
+  weekday[1] = "Mon";
+  weekday[2] = "Tue";
+  weekday[3] = "Wed";
+  weekday[4] = "Thu";
+  weekday[5] = "Fri";
+  weekday[6] = "Sat";
+  return weekday[d.getDay()];
+}
+
+function display(results,city){
+  $('#loc').replaceWith('<h3 id="loc" name="loc">Forecast for ' + city + '</h3>');
+
+  var display = '<div id="display" name="display" class="ui-grid-d  addBorder" align="center">';
+
+  display += '<div class="ui-block-a"><b>' + results[0].time.toUpperCase() + '</b><br/><br/>' + results[0].temp + '&deg;<br/><img src="http://openweathermap.org/img/w/' + results[0].icon +'.png"><br/>' + results[0].weather.toUpperCase() + '<br/><br/>' + results[0].desc + '</div>';
+  display += '<div class="ui-block-b"><b>' + results[1].time.toUpperCase() + '</b><br/><br/>' + results[1].temp + '&deg;<br/><img src="http://openweathermap.org/img/w/' + results[1].icon +'.png"><br/>' + results[1].weather.toUpperCase() + '<br/><br/>' + results[1].desc + '</div>';
+  display += '<div class="ui-block-c"><b>' + results[2].time.toUpperCase() + '</b><br/><br/>' + results[2].temp + '&deg;<br/><img src="http://openweathermap.org/img/w/' + results[2].icon +'.png"><br/>' + results[2].weather.toUpperCase() + '<br/><br/>' + results[2].desc + '</div>';
+  display += '<div class="ui-block-d"><b>' + results[3].time.toUpperCase() + '</b><br/><br/>' + results[3].temp + '&deg;<br/><img src="http://openweathermap.org/img/w/' + results[3].icon +'.png"><br/>' + results[3].weather.toUpperCase() + '<br/><br/>' + results[3].desc + '</div>';
+  display += '<div class="ui-block-e"><b>' + results[4].time.toUpperCase() + '</b><br/><br/>' + results[4].temp + '&deg;<br/><img src="http://openweathermap.org/img/w/' + results[4].icon +'.png"><br/>' + results[4].weather.toUpperCase() + '<br/><br/>' + results[4].desc + '</div>';
+
+  display += '</div>';
+
+  $('#display').replaceWith(display);
+}
+
+function display1(results, city){
 $('#loc').replaceWith('<h3 id="loc" name="loc">Forecast for ' + city + '</h3>');
 
   var display = '<table align="center" id="display" name="display" data-role="table" class="ui-responsive"><tr><th>Forecast</th><th>Temperature</th></tr>';
